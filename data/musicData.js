cb3d530e3659dd2abaa4e818d493363ff6ec3b0f
robotchickn <robotchicknbackup1@gmail.com>
2025-03-25 18:59:42 +0530

    changed music recs

--- data/musicData.js
+++ data/musicData.js
@@ -4,7 +4,7 @@ const musicData = {
       title: 'Ants From Up There',
       artist: 'Black Country, New Road',
       description:
-        'A beautifully crafted art-rock album with intricate instrumentation and emotional depth.',
+        'My first love with lengthy instrumentals, elegant lyrics and complex chord progressions.',
       tags: ['Art Rock', 'Experimental', 'Post-Rock'],
       href: 'https://open.spotify.com/album/1dOZgwWjGZUnT6Q1Xck8aJ',
       poster: '/static/images/music/ants-from-up-there.jpg',
@@ -13,7 +13,7 @@ const musicData = {
       title: 'Nuvvostanante Nenoddantana',
       artist: 'Devi Sri Prasad (DSP)',
       description:
-        'A cult classic Telugu film soundtrack with unforgettable melodies and rich instrumentation.',
+        'DSP does an excellent here! All the songs convey a story of their own along with the wonderful cinematography of Prabhu Deva',
       tags: ['Soundtrack', 'Tollywood', 'Romantic'],
       href: 'https://open.spotify.com/album/1bA1qN7vUPmFY7gA8RmAmn',
       poster: '/static/images/music/nuvvostanante.jpg',
@@ -21,7 +21,7 @@ const musicData = {
     {
       title: 'Incredibad',
       artist: 'The Lonely Island',
-      description: 'A hilarious and surprisingly catchy comedy-rap album from the SNL comedy trio.',
+      description: 'The Greatest Band Ever. Period.',
       tags: ['Comedy', 'Hip-Hop', 'Rap'],
       href: 'https://open.spotify.com/album/6zEgnpM0qYmHLDnh8WPejL',
       poster: '/static/images/music/incredibad.jpg',
@@ -30,8 +30,8 @@ const musicData = {
       title: 'Chromatic Grammatic',
       artist: 'Lydian Nadhaswaram',
       description:
-        'A mesmerizing blend of Western classical, jazz, and Indian influences from a young prodigy.',
-      tags: ['Jazz Fusion', 'Carnatic', 'Instrumental'],
+        "As much as he gets the spotlight for his piano skills, I still feel he is extremely underrated. By the way in this album, there's a song for every key centre!",
+      tags: ['Jazz Fusion', 'Instrumental'],
       href: 'https://open.spotify.com/album/4b8lB5aB85zi2xFkgNSlF3',
       poster: '/static/images/music/chromatic-grammatic.jpg',
     },
@@ -39,7 +39,7 @@ const musicData = {
       title: 'The Carnatic Jazz Experiment',
       artist: 'Toby Wren',
       description:
-        'An innovative mix of Carnatic classical music and contemporary jazz improvisation.',
+        'An intresting mix of Carnatic classical music and contemporary jazz improvisation.',
       tags: ['Carnatic', 'Jazz', 'Fusion'],
       href: 'https://open.spotify.com/album/3pDkjs4WLUHaTfPYWjbZok',
       poster: '/static/images/music/carnatic-jazz-experiment.jpg',
@@ -47,8 +47,8 @@ const musicData = {
     {
       title: 'Nakshatra',
       artist: 'Trina Basu',
-      description: 'A stunning fusion of Indian classical music with Western jazz influences.',
-      tags: ['Indian Classical', 'Jazz', 'Fusion'],
+      description: 'I have cried several times listening to `Tempest` several times 😅',
+      tags: ['Indian Classical', 'Improvisation'],
       href: 'https://open.spotify.com/album/7CFcDykSefSwpFVA5BbrEf',
       poster: '/static/images/music/nakshatra.jpg',
     },
@@ -58,7 +58,7 @@ const musicData = {
     {
       title: 'The Trio (Live, 1973)',
       artist: 'Oscar Peterson',
-      description: 'A legendary live jazz performance by one of the greatest pianists of all time.',
+      description: 'He never played the piano. He always just played with a toy!',
       tags: ['Jazz', 'Live', 'Piano'],
       href: 'https://open.spotify.com/album/6uN8CqB9nMccSJfSMywJTS',
       poster: '/static/images/music/the-trio.jpg',
